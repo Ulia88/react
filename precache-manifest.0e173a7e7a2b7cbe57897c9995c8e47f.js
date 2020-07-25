145607e64f81fc26e22ebe887d7beb2df5625173
Ulia88 <ylka0508@gmail.com>
2020-07-24 18:11:39 -0700

    Updates

---
+++ precache-manifest.0e173a7e7a2b7cbe57897c9995c8e47f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87b9c38864dfbdd53ecbd1d51abe660",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "5b64e80574db445d47e8",
+    "url": "/react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4127bd5b6ceb4c6fa7f1",
+    "url": "/react/static/js/2.1297a977.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.1297a977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b64e80574db445d47e8",
+    "url": "/react/static/js/main.8b72f308.chunk.js"
+  },
+  {
+    "revision": "addd72d843a66e73b291",
+    "url": "/react/static/js/runtime-main.cc189ae4.js"
+  }
+]);
